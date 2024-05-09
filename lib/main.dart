ccf4764ac1db5cb991323d65485e70f92f5edcdc
mdbicode <mdbahrulilmi@gmail.com>
2024-05-09 19:53:29 +0700

    fix scroll and add top categories

--- lib/main.dart
+++ lib/main.dart
@@ -57,163 +57,199 @@ class _MyHomePageState extends State<MyHomePage> {
           fontSize: 16,
           fontWeight: FontWeight.bold,
         ), 
-        child: Column(
-          children: [
-            // Search Bar
-            const Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 20.0),
-            child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0) )
-              ),
-              hintText: 'Search the podcast here',
-              prefixIcon: Icon(Icons.search)
-              ),
-            ),
-                          ),
-            // Promoted Podcast
-            Align(
-              alignment: Alignment.centerLeft,
+        child: 
+        SingleChildScrollView(
+  child: Column(
+    children: [
+      // Search Bar
+      Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 10.0, 30.0, 20.0),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25.0))),
+            hintText: 'Search the podcast here',
+            prefixIcon: Icon(Icons.search),
+          ),
+        ),
+      ),
+      // Promoted Podcast
+      Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Promoted Podcast', textAlign: TextAlign.left),
+        ),
+      ),
+      // Promoted Podcast List
+      SizedBox(
+        height: 200,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10 ,left: 10),
               child: Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text('Promoted Podcast', textAlign: TextAlign.left),
-              )
-              ),
-            SizedBox(
-            height: 200,
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: Container(
-                        width: 300,
-                        color: Colors.grey,                      
-                        ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Container(
-                        width: 300,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Container(
-                        width: 300,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
+                width: 300,
+                color: Colors.grey,
               ),
             ),
-            Align(
-              alignment: Alignment.centerLeft,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text('Trending Podcast', textAlign: TextAlign.left),
-              )
+                width: 300,
+                color: Colors.grey,
               ),
-            Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    // Image
-                    const Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child:  SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: ColoredBox(color: Colors.grey)
-                        ),),
-                    // Title n Desc
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Denny Sumargo',
-                        style: TextStyle(fontSize: 14),
-                        ),
-                        Text('@curhatbang',
-                        style: TextStyle(fontSize: 14,fontWeight: FontWeight.normal),
-                        ),
-                        Text('21 Min',
-                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-                        ),
-                      ],
-                    ),
-                      ],
-                    ),
-                    // Play
-                    Container(
-                      padding: EdgeInsets.only(right: 40),
-                      child: Icon(Icons.play_circle_fill,size: 50),
-                    )
-                  ],
-                )
-              ],
-              
             ),
-
-            Align(
-              alignment: Alignment.centerLeft,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text('Continue Podcast', textAlign: TextAlign.left),
-              )
+                width: 300,
+                color: Colors.grey,
+              ),
             ),
-            Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    // Image
-                    const Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child:  SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: ColoredBox(color: Colors.grey)
-                        ),),
-                    // Title n Desc
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Denny Sumargo',
-                        style: TextStyle(fontSize: 14),
-                        ),
-                        Text('@curhatbang',
-                        style: TextStyle(fontSize: 14,fontWeight: FontWeight.normal),
-                        ),
-                        Text('21 Min',
-                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
-                        ),
-                      ],
-                    ),
-                      ],
-                    ),
-                    // Play
-                    Container(
-                      padding: EdgeInsets.only(right: 40),
-                      child: Icon(Icons.play_circle_fill,size: 50),
-                    )
-                  ],
-                )
-              ],
-              
-            ),
-
-
           ],
         ),
-      ));
+      ),
+      // Trending Podcasts
+      Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Trending Podcasts', textAlign: TextAlign.left),
+        ),
+      ),
+      // Trending Podcasts List
+      Column(
+        children: <Widget>[
+          _buildListItem(
+            title: 'Denny Sumargo',
+            username: '@curhatbang',
+            duration: '21 Minutes',
+          ),
+          _buildListItem(
+            title: 'Close of the Door',
+            username: '@corbuzier',
+            duration: '17 Minutes',
+          ),
+          _buildListItem(
+            title: 'Raditya Dika',
+            username: '@radityadika',
+            duration: '43 Minutes',
+          ),
+        ],
+      ),
+      // Continue Podcast
+      Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Continue Podcast', textAlign: TextAlign.left),
+        ),
+      ),
+      // Continue Podcast List
+      Column(
+        children: <Widget>[
+          _buildListItem(
+            title: 'Denny Sumargo',
+            username: '@curhatbang',
+            duration: '21 Min',
+          ),
+          _buildListItem(
+            title: 'Close of the Door',
+            username: '@corbuzier',
+            duration: '21 Min',
+          ),
+          _buildListItem(
+            title: 'Raditya Dika',
+            username: '@radityadika',
+            duration: '21 Min',
+          ),
+        ],
+      ),
+      // Top Categories
+      Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Text('Top Categories', textAlign: TextAlign.left),
+        ),
+      ),
+      // Top Categories List
+      SizedBox(
+        height: 100,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 10 ,left: 10),
+              child: Container(
+                width: 200,
+                color: Colors.grey,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                width: 200,
+                color: Colors.grey,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                width: 200,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+      
+    ],
+  ),
+),
+));
   }
+}
+Widget _buildListItem({required String title, required String username, required String duration}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      // Image
+      const Padding(
+        padding: const EdgeInsets.all(10),
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: ColoredBox(color: Colors.grey),
+        ),
+      ),
+      // Title n Desc
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: const TextStyle(fontSize: 14),
+            ),
+            Text(
+              username,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+            Text(
+              duration,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+            ),
+          ],
+        ),
+      ),
+      // Play
+      Container(
+        padding: const EdgeInsets.only(right: 40),
+        child: const Icon(Icons.play_circle_fill, size: 50),
+      ),
+    ],
+  );
 }
